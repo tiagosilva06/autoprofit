fefc16903a6ac00356619cbc58659b615365e2b4
Tiago Santos <tiagosilva9278@gmail.com>
2026-06-16 23:47:00 -0300

    feat: add models seed data

---
+++ sql/seed.sql
@@ -0,0 +1,10 @@
+-- AutoProfit Analytics
+-- Seed data
+
+INSERT INTO models VALUES
+(1, 'FCA-PULSE', 'Fiat Pulse',   'Fiat',    'suv',    1000, 2021),
+(2, 'FCA-ARGO',  'Fiat Argo',    'Fiat',    'hatch',  1000, 2017),
+(3, 'RAM-1500',  'RAM 1500',     'RAM',     'pickup', 5700, 2019),
+(4, 'JEP-COMP',  'Jeep Compass', 'Jeep',    'suv',    1300, 2016),
+(5, 'CIT-C4',    'Citroen C4',   'Citroen', 'sedan',  1600, 2021),
+(6, 'PEG-2008',  'Peugeot 2008', 'Peugeot', 'suv',    1200, 2019);
